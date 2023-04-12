f50c662f60e1123c6ef89d69b33ef5bb8ce36d64
Ken Kudo <kkudo224@gmail.com>
2023-04-12 17:25:15 +0000

    DONE

--- src/main/java/MaxMinusMin.java
+++ src/main/java/MaxMinusMin.java
@@ -10,6 +10,16 @@ public class MaxMinusMin {
      * @return the max value of nums minus the min value of nums.
      */
     public int difference(List<Integer> nums){
-        return 0;
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int num : nums) {
+            if (num>max) {
+                max = num;
+            }
+            if (num<min) {
+                min = num;
+            }
+        }
+        return max - min;
     }
 }
